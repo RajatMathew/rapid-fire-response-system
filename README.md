4e4041784ac83bcb10058f6ea76f388d4b10a79c
rajatmathew <rajatxmathew@gmail.com>
2023-10-08 23:53:12 +0530

    Added readme, updated user frontend

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rapid Fire Response System
+Built as part of NASA Space Apps Challenge.
+
+
+### Drive Link
+https://drive.google.com/drive/folders/1q9Qpf2Gorlf0CiSQrwzVLT8V7usUSDzr?usp=sharing
--- user-frontend/src/App.jsx
+++ user-frontend/src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   return (
     <>
     <MapExplorer/>
+    <h2 className='mt-[100vh] text-2xl font-bold'>Report fire</h2>
+    <input className='p-4 border-2 rounded-md' type="text" placeholder='Location' />
+      <br />
+    <textarea className='p-4 border-2 rounded-md' type="text" placeholder='Add details' />
+    <br />
+    <button className='mb-96 bg-teal-600 p-5 text=white text-lg text-white'>Submit</button>
     </>
   )
 }
